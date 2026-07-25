26e0e46acdb5c635282a4ea02efa717995146840
kenkiren <deveshshyamgupta@gmail.com>
2026-07-26 00:50:04 +0530

    first draft is done by me

--- my-todo/src/App.jsx
+++ my-todo/src/App.jsx
@@ -1,14 +1,35 @@
 import { useState } from 'react'
 
 function App() {
-  const [count, setCount] = useState(0)
+  // const [count, setCount] = useState(0)
+  const [todo,setTodo]= useState("");
+  const[todos,setTodos] =useState([]);
 
+    function addtodo(){
+      const newtodo={
+        text: todo,
+        completed: false
+      }
+      setTodos([...todos, newtodo]);
+      setTodo("");
+    }
   return (
     
   <>
         <h1>MY TODO APPLICATION</h1>
+        <input
+        type="text"
+        placeholder= "type smth"
+        value={todo}
+        onChange={(e)=>setTodo(e.target.value)} />
+        
+        <button onClick={addtodo}> ADD</button>
 
-      
+        <ul>
+          { todos.map((item,index)=>(
+            <li key={index} > {item.text}    </li>
+          ))  }
+        </ul>
 
     </>
   )

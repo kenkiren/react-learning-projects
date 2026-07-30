ca05167e4990a2064b546d7a6646577a95409530
kenkiren <deveshshyamgupta@gmail.com>
2026-07-30 23:34:18 +0530

    base is done. it shows git search result .

--- GithubSearch/src/App.jsx
+++ GithubSearch/src/App.jsx
@@ -1,11 +1,33 @@
 import { useState } from 'react'
+import { useEffect } from 'react'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [username, setUsername] = useState('');
+  const[user,setUser]= useState(null);
+
+  useEffect(() => {
+      fetch(`https://api.github.com/users/${username}`)
+    .then((res) => res.json())
+    .then((data) => setUser(data))
+}, [username]);
+
 
   return (
     <>
-    <h1>Another Learning Project</h1>
+    <h1>Another Learning Project- Github Search</h1>
+    <input type="text"
+    placeholder="type here"
+    value={username}
+    onChange={(e) => setUsername(e.target.value)} />
+  
+   {user && (
+     <div>
+       <img src={user.avatar_url} alt={user.name} />
+       <h2>{user.name}</h2>
+       <p>{user.bio}</p>
+     </div>
+   )}
+
     </>
   )
 }
